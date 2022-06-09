e3c559fb0044501ca7e0a80c2fe98b7ce65118b9
Julia Djomina <julia.djomina@gmail.com>
2022-06-09 12:33:05 +0300

    Service urls are fixed

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,28 +1,31 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Product} from "../../models/product";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  products: string = "/products/";
+
   constructor(private http: HttpClient) {
   }
 
   createProduct(product: Product) {
-    return this.http.post<any>("http://localhost:8080/products/", product)
+    return this.http.post<any>(`${environment.API_BASE_URL}${this.products}`, product)
   }
 
   updateProduct(product: Product) {
-    return this.http.put("http://localhost:8080/products/", product)
+    return this.http.put(`${environment.API_BASE_URL}${this.products}`, product)
   }
 
   getProductList() {
-    return this.http.get("http://localhost:8080/products")
+    return this.http.get(`${environment.API_BASE_URL}${this.products}`)
   }
 
   deleteProduct(id: number) {
-    return this.http.delete("http://localhost:8080/products/" + id)
+    return this.http.delete(`${environment.API_BASE_URL}${this.products}${id}`)
   }
 
 }
--- src/app/service/storage.service.ts
+++ src/app/service/storage.service.ts
@@ -1,37 +1,41 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Storage} from "../../models/storage";
+import {environment} from "../../environments/environment";
+import {stringify} from "@angular/compiler/src/util";
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
+  storages: string = "/storages/";
+  products: string = "/products/";
 
   constructor(private http: HttpClient) {
   }
 
-  createStorage(data: any) {
-    return this.http.post<any>("http://localhost:8080/storages/", data)
+  createStorage(storage: Storage) {
+    return this.http.post<any>(`${environment.API_BASE_URL}${this.storages}`, storage)
   }
 
   updateStorage(storage: Storage) {
-    return this.http.put("http://localhost:8080/storages/", storage)
+    return this.http.put(`${environment.API_BASE_URL}${this.storages}`, storage)
   }
 
   getStorageList() {
-    return this.http.get("http://localhost:8080/storages")
+    return this.http.get(`${environment.API_BASE_URL}${this.storages}`)
   }
 
   getStorageById(id: number) {
-    return this.http.get("http://localhost:8080/storages/" + id)
+    return this.http.get(`${environment.API_BASE_URL}${this.storages}${id}`)
   }
 
   deleteStorage(id: number) {
-    return this.http.delete("http://localhost:8080/storages/" + id)
+    return this.http.delete(`${environment.API_BASE_URL}${this.storages}${id}`)
   }
 
   getStorageProducts(id: number) {
-    return this.http.get("http://localhost:8080/storages/" + id + "/products/")
+    return this.http.get(`${environment.API_BASE_URL}${this.storages}${id}${this.products}`)
   }
 
 }
